b80e586fa6e37edc5831ea66dbac67e3407f9588
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-19 20:15:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  let paragraph = document.getElementById("text");
+  paragraph.innerHTML= ""
+  console.log("The DOM has loaded");
+});
